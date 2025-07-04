371799d3e3e62a27e60ec4007da2e82a2533de28
devvictor <victorritossantos@gmail.com>
2025-07-04 15:24:55 -0300

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if janela == "Cadastro":
 
     st.header("Fechamento Diário")
 
-    st.subheader("Preechimento Movel")
+    st.subheader("Campo de preenchimento Movel")
 
     op = ["SHOPPING IGUATEMI1" , "SHOPPING IGUATEMI2" , "SHOPPING LAPA" , "SHOPPING BELA VISTA" , "SHOPPING SALVADOR"]
 
@@ -23,46 +23,82 @@ if janela == "Cadastro":
     
     data = st.date_input("Data",value = datetime.date.today())
 
+    # Linha 1 - CAMPOS PÓS
+    col1_controle, col2_controle, col3_controle, col4_controle = st.columns(4)
+
+    with col1_controle:
+        alta = st.number_input("ALTA PÓS RECEITA", help="QUANTIDADE DE ALTA PÓS PURO (RECEITA)",step=1)
+
+    with col2_controle:
+        alta2 = st.number_input("ALTA PÓS FÍSICO", help="QUANTIDADE DE ALTA PÓS PURO (FÍSICO)",step=1)
+
+    with col3_controle:
+        migra = st.number_input("MIGRA PRÉ RECEITA", help="QUANTIDADE DE MIGRA PRÉ/CTRL PURO (RECEITA)",step=1)
+
+    with col4_controle:
+        migra2 = st.number_input("MIGRA PRÉ FÍSICO", help="QUANTIDADE DE MIGRA PRÉ/CTRL PURO (FÍSICO)",step=1)
 
 
-# Linha 1 - CAMPOS PÓS
-col1_controle, col2_controle, col3_controle, col4_controle = st.columns(4)
-
-with col1_controle:
-    alta = st.text_input("ALTA PÓS RECEITA", help="ALTA PÓS PURO (RECEITA)")
-
-with col2_controle:
-    alta2 = st.text_input("ALTA PÓS FÍSICO", help="ALTA PÓS PURO (FÍSICO)")
-
-with col3_controle:
-    migra = st.text_input("MIGRA PRÉ RECEITA", help="MIGRA PRÉ/CTRL PURO (RECEITA)")
-
-with col4_controle:
-    migra2 = st.text_input("MIGRA PRÉ FÍSICO", help="MIGRA PRÉ/CTRL PURO (FÍSICO)")
+    # Linha 2 - PORTABILIDADES
+    portabilidade = st.number_input("Portabilidade", help="QUANTIDADE DE PORTABILIDADE",step=1)
 
 
-# Linha 2 - PORTABILIDADES
-st.text_input("Portabilidade", help="Quantidade de Portabilitade")
+    # Linha 3 - CAMPOS CONTROLE
+    col5_controle, col6_controle, col7_controle , col8_controle = st.columns(4)
 
+    with col5_controle:
+        controle = st.number_input("CONTROLE RECEITA", help="QUANTIDADE DE ALTA CONTROLE (RECEITA)",step=1)
 
-# Linha 3 - CAMPOS CONTROLE
-col5_controle, col6_controle, col7_controle , col8_controle = st.columns(4)
+    with col6_controle:
+        controle2 = st.number_input("CONTROLE FÍSICO", help="QUANTIDADE DE ALTA CONTROLE (FÍSICO)",step=1)
 
-with col5_controle:
-    controle = st.text_input("CONTROLE RECEITA", help="ALTA CONTROLE (RECEITA)")
+    with col7_controle:
+        migra_controle = st.number_input("MIGRA PRÉ RECEITA", help="QUANTIDADE DE MIGRA CTRL PRÉ (RECEITA)",step=1)
 
-with col6_controle:
-    controle2 = st.text_input("CONTROLE FÍSICO", help="ALTA CONTROLE (FÍSICO)")
+    with col8_controle:
+        migra_controle2 = st.number_input("MIGRA PRÉ FÍSICO", help="QUANTIDADE DE MIGRA CTRL PRÉ (FÍSICO)",step=1)
 
-with col7_controle:
-    migra_controle = st.text_input("MIGRA CRL PRÉ RECEITA", help="MIGRA CTRL PRÉ (RECEITA)")
+    #Segundo campo de preenchimento 
 
-with col8_controle:
-    migra_controle = st.text_input("MIGRA CRL PRÉ FÍSICO", help="MIGRA CTRL PRÉ (Físico)")
+    st.subheader("Campo de preenchimento Fixa")
 
+    #PRIMEIRA LINHA
+
+    col9_fixa, col10_fixa, col11_fixa , col12_fixa = st.columns(4)
+
+    with col9_fixa:
+        fixaet = st.number_input("RECEITA FIXA(EXT)", help= " QUANTIDADE DE RECEITA FIXA(VENDA EXT)",step=1 )
+
+    with col10_fixa:
+        fixaet = st.number_input("FÍSICO FIXA(EXT)", help= " QUANTIDADE DE FÍSICO FIXA(VENDA EXT)",step=1 )
+
+    with col11_fixa:
+        fixaet3 = st.number_input("FÍSICO VIVO TOTAL(EXT)", help= " QUANTIDADE DE FÍSICO VIVO TOTAL FIXA(VENDA EXT)" ,step=1)
+
+    with col12_fixa:
+        fixaet4 = st.number_input("B2B FÍXA(FÍSICO)", help= " QUANTIDADE DE B2B FÍXA(FÍSICO)",step=1)
+
+    #SEGUNDA LINHA
+
+    col13_fixa, col14_fixa, col15_fixa , col16_fixa = st.columns(4)
+        
+    with col13_fixa:
+        fixal = st.number_input("RECEITA FIXA(LOJA)",help="QUANTIDADE DE RECEITA FIXA(LOJA)",step=1)
+
+    with col14_fixa:
+        fixal2 = st.number_input("FÍSICO FIXA(LOJA)",help="QUANTIDADE DE FÍSICO FIXA(LOJA)",step=1)
+    
+    with col15_fixa:
+        fixal3 = st.number_input("FÍSICO VIVO TOTAL(LOJA)",help="QUANTIDADE DE FÍSICO VIVO TOTAL(LOJA)",step=1)
+
+    with col16_fixa:
+        fixal4 = st.number_input("B2B MOVEL(FÍSICO)",help="QUANTIDADE DE B2B MOVEL(FÍSICO)",step=1)
+
+    #TERCEIRO CAMPO DE PREEENCHIMENTO 
 
     
-if st.button("Cadastrar"):
+
+    if st.button("Cadastrar"):
             
             if nome and data and alta and alta2:
                 
@@ -72,6 +108,13 @@ if st.button("Cadastrar"):
                                                                     "ALTA PÓS PURO (FÍSICO) " : [alta2],
                                                                     "MIGRA PRÉ/CTRL PURO(RECEITA)" : [migra],
                                                                     "MIGRA PRÉ/CTRL PURO(FÍSICO)" : [migra2], 
+                                                                    "ALTA CONTROLE (RECEITA)" : [controle],
+                                                                    "ALTA CONTROLE (FÍSICO)" : [controle2] , 
+                                                                    "MIGRA CTRL PRÉ (RECEITA)" : [migra_controle] , 
+                                                                    "MIGRA CTRL PRÉ (FÍSICO)" : [migra_controle2] , 
+                                                                    "PORTABILIDADE" : [portabilidade]
+
+
                                                                     })
                 st.session_state.nova_planilha = pd.read_excel(st.session_state.arquivo)
                 st.session_state.novo = pd.concat([st.session_state.nova_planilha, st.session_state.planilha_fechamento], ignore_index=True)
@@ -80,6 +123,13 @@ if st.button("Cadastrar"):
 
             else:
                 st.error("Preencha todos os dados solicitados! ")
+
+
+
+
+
+     
+    
 
 
         
